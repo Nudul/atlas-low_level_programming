beb076ea00b41ac786bd0095d1adfd65c0401335
Nudul <cjsuntken@gmail.com>
2024-05-18 18:11:47 +0000

    t

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* Prints the string from the prompt */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/* Return 0 to indicate success */
+	return (0);
+}
