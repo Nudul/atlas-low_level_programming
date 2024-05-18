8dc61c09e091aaf6e0c9b5dba3ae40f653e5b7a7
Nudul <cjsuntken@gmail.com>
2024-05-18 18:14:43 +0000

    tglglgllglg

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-/* Description: This function prints the string exactly as written followed by a newline */
+/* Description: This function prints the string followed by newline */
 int main(void)
 {
 	/* Prints the string from the prompt */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	/* Return 0 to indicate success */
 	return (0);
 }
