4afa1f5df91aaac82c7e55680733cf550b268c5f
Nudul <cjsuntken@gmail.com>
2024-05-18 17:30:10 +0000

    t

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-/**
- * Main will print a string with the quote at the beginning included
- */
 
+/* Main will print a string with the quote at the beginning included */
 int main(void)
 {
+	/* Prints the string with puts */
 	puts("\"Programming is like building a multilingual puzzle");
-	/* Literally prints the string */
+	/* Returns to 0 to check success */
 	return (0);
-	/* Returns to 0 for success */
 }
