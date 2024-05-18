70a50ffa233eb47fe69c7354d06b1bd62220dc45
Nudul <cjsuntken@gmail.com>
2024-05-18 18:35:33 +0000

    t

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main (void)
 {
 	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
@@ -7,7 +6,6 @@ int main (void)
 	printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(long));
 	printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(long long));
 	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
-
-	// Return 0 for successfull execute
+	/* Return 0 for successfull execute */
 	return (0);
 }
