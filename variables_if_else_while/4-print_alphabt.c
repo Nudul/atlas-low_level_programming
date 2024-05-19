6ee47bd69e33eb9dd6c39a37ca9fa8d7a32e9f61
Nudul <cjsuntken@gmail.com>
2024-05-19 20:01:50 +0000

    t

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - blahblah
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (char c = 'a'; c!= 'q'; c++)
+	{
+		putchar(c);
+	}
+	for (; c!= 'e'; c++)
+	{
+		putchar(c);
+	}
+	
+	return (0);
+}
