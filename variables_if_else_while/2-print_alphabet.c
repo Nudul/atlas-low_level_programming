c208297ec8ce7b9721152d126db1f5f29db3a36e
Nudul <cjsuntken@gmail.com>
2024-05-19 19:28:17 +0000

    t

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - blahblahblah
+ *
+ * Return: 0
+ */
+int main()
+{
+	int i;
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
