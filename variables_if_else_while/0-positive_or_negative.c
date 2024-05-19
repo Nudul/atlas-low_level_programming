8f909cc17d73c775dd16756664bbd77812f945cc
Nudul <cjsuntken@gmail.com>
2024-05-19 18:46:14 +0000

    t

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
 
 /* Checks the value of n */
 	if (n > 0) {
-		printf("The number %d is positive\n", n);
-	} else if (n == 0) {
-		printf("The number %d is zero \n", n);
+		printf("%d is positive\n", n);
+	} else if (n < 0) {
+		printf("%d is negative\n", n);
 	} else {
-		printf("The number %d is negative\n", n);
+		printf("0 is zero\n");
 	}
 
 	return 0;
