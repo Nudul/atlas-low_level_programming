6c1deb97a6c55fd84a90378c57b9071d6c745638
Nudul <cjsuntken@gmail.com>
2024-05-19 19:41:56 +0000

    t

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,15 @@
 
 int main(void)
 {
-	for (int i = 0; i < 26; ++i)
+	for (char c = 'a'; c <= 'z'; c++)
 	{
-		putchar('a' + i);
+		putchar(c);
 	}
 
-	for (int i = 0; i < 26; ++i)
+	for (char c = 'A'; c <= 'Z'; c++)
 	{
-		putchar('A' + i);
+		putchar(c);
 	}
-
-	putchar('\n'');
 
 	return (0);
 }
