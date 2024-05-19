721dcf201437127ef613e17bda00dcccae41d4b2
Nudul <cjsuntken@gmail.com>
2024-05-19 19:34:08 +0000

    t

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - asdfghjkl
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 26; ++i)
+	{
+		putchar('a' + i);
+	}
+
+	for (int i = 0; i < 26; ++i)
+	{
+		putchar('A' + i);
+	}
+
+	putchar('\n'');
+
+	return (0);
+}
