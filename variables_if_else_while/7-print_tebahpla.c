9f01f2321167fc4020eb612b9b2a43b8cec56943
Nudul <cjsuntken@gmail.com>
2024-05-20 20:42:32 +0000

    t

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - blahblah
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return(0);
+}
