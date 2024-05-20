89d00a107b160b4c38cbb28b948f6dad86f1362e
Nudul <cjsuntken@gmail.com>
2024-05-20 23:15:52 +0000

    t

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,10 @@
 int main(void) {
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (int i = 0; i < 10; i++)
+	int; i, j
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j < 26; j++)
+		for (j = 0; j < 26; j++)
 		{
 			_putchar(alphabet[j]);
 		}
