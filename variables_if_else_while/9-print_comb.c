3fba3b9e60fd8e2c78389644a8d45b876be6f227
Nudul <cjsuntken@gmail.com>
2024-05-20 21:26:06 +0000

    t

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,30 +9,21 @@
 int main(void)
 {
 	int digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int buffer[11]; 
-	int index = 0;
-
-	int i, j;
-
-	for (i = 0; i <= 9; ++i)
-	{
-		buffer[index++] = digits[i];
-
-		for (j = 0; j < index; ++j)
-		{
-			putchar(buffer[j] + '0');
-			if (j < index - 1)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	int size = sizeof(digits) / sizeof(digits[0]);
 	
+	int i;
 
-		index = 0;
-
+	for (i = 0; i < size ++i)
+	{
+		printf("%d", digits[i]);
+		if (i < size - 1)
+		{
+			printf(", ");
+		}
 	}
-	putchar('\n');
 
-	return (0);
+printf("\n");
+
+return (0);
+
 }
