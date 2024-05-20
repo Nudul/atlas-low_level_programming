57132fd5c190d00d7531d3a817c416a6686fa74f
Nudul <cjsuntken@gmail.com>
2024-05-20 23:56:28 +0000

    t

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		write(1, "+", 1);
+	return (1);
+	}
+else if (n == 0)
+		{
+	write(1, "0", 1);
+	return (0);
+		}
+else
+	}
+	write(1, "-", 1);
+	return (-1);
+	}
+}
