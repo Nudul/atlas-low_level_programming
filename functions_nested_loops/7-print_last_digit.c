44243899a0e4be19ff6e3e8d5aaf9ec60a5ed6d4
Nudul <cjsuntken@gmail.com>
2024-05-21 00:14:50 +0000

    t

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,17 @@
 #include "main.h"
 #include <unistd.h>
 
+int main(void) 
+{
+	int r = print_lastdigit(98)
+		return (0);
+}
+
 int print_last_digit(int n)
 {
 	int lastDigit;
 
 	lastDigit = n % 10;
-
 	write(1, &lastDigit, 1);
-
 	return lastDigit;
 }
