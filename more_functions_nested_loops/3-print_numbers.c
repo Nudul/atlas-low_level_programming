7237a5f1df4f979666a03c65bfb8f743e7df7994
Nudul <cjsuntken@gmail.com>
2024-05-26 20:14:24 +0000

    t

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - is awesome
+ *
+ *
+ *
+ * Return: 1 or 0
+ */
+void print_numbers(void)
+{
+	_putchar(numbers[0]);
+	for (int i = 1; i < 10; i++)
+	{
+		_putchar(numbers[i]);
+	}
+}
