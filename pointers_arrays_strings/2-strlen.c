fa3acfea5a47269d73da670be7ea26ae1063d7fa
Nudul <cjsuntken@gmail.com>
2024-05-27 22:32:55 +0000

    t

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - dldfskf
+ * 
+ * @s: cool letter
+ * Return: 0 or 1
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+
+		return (c);
+}
