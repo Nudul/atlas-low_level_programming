eaa3dad4e4433367beff875868e9058b277f16ea
Nudul <cjsuntken@gmail.com>
2024-05-27 21:03:40 +0000

    t

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,9 @@
 /**
  *
  * _putchar - is very cool
- *
+ * 
+ * @i: is awesome
+ * @j: is very cool
  *
  * Return: 1 or 0
  */
@@ -10,12 +12,18 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	i = 0;
+	while (i < 10)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-			_putchar(j + '0');
-		}
-		_putchar('\n');
+	j = 0;
+	while (j < 15)
+	{
+		if (j > 9)
+			_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
+		j++;
+	}
+	i++;
+	_putchar('\n');
 	}
 }
