4c3bd70981ab5f09450c4b1fa95abb97d4143cf6
Nudul <cjsuntken@gmail.com>
2024-05-27 23:09:34 +0000

    t

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - cool function
+ * @n: blahblah
+ * @a: blahblah
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		print("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
