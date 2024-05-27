de3a2ff9daada6db3cd3c78b00f24d9b7e3aecf5
Nudul <cjsuntken@gmail.com>
2024-05-27 20:31:26 +0000

    t

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - very cool function
+ *
+ *
+ *
+ * Return: 1 or 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
+		
