5ccfcf2d14f4a8ebd0249d7b25f5c5bd66b43388
Nudul <cjsuntken@gmail.com>
2024-05-27 20:45:39 +0000

    t

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,10 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j =0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
-
-	return (0);
-} 
+}
