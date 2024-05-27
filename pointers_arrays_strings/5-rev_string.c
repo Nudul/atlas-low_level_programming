38c4bd2d4505874d66ac9fcbdc31a41104f6fa24
Nudul <cjsuntken@gmail.com>
2024-05-27 22:56:59 +0000

    t

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - what do u think it does
+ *
+ * @s: blahblahblah
+ */
+void rev_string(char *s)
+{
+	int i, n, j;
+	char h;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	j = i;
+	
+	for (i--, n = 0; n < 1 / 2; i--, n++)
+	{
+		h = s[n];
+		s[n] = s[i];
+		s[i] = h;
+	}
+}
