b84313cadff1b7fcaaad49634b840a9783f8a65b
Nudul <cjsuntken@gmail.com>
2024-05-31 23:07:44 +0000

    cleanup formatting

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,14 +1,16 @@
 #include "main.h"
-/*
+/**
+ * reverse_array - cool thing
  */
 void reverse_array(int *a, int n)
 {
 	int start = 0;
-	int end = n -1;
+	int end = n - 1;
 
 	while (start < end)
 	{
 		int temp = a[start];
+		
 		a[start] = a[end];
 		a[end] = temp;
 
