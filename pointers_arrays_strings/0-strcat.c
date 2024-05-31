468581f0c64aa3cd6c6d0c93d050bf34a664bba6
Nudul <cjsuntken@gmail.com>
2024-05-31 21:22:15 +0000

    t

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _strcat is very cool
- *
+ * _strcat - is very cool
+ * @dest: cool word
+ * @src: even cooler
  * Return: dest stuff
  */
 char *_strcat(char *dest, char *src)
