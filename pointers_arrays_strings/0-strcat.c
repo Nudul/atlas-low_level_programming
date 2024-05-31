4c8ed45b654823819d5c6c57782a1dc0531d1f56
Nudul <cjsuntken@gmail.com>
2024-05-31 21:18:42 +0000

    t

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * _strcat is very cool
  *
@@ -7,7 +7,6 @@ include "main.h"
 char *_strcat(char *dest, char *src);
 {
 
-// find end of dest string
 char *d = dest;
 
 while (*d)
@@ -15,14 +14,12 @@ while (*d)
 d++;
 }
 
-// copy src string starting from first character to end of dest
 while (*src)
 {
 	*d++ = *src++;
 }
 
-// Null terminator at end of string
 *d = '\0';
 
-return dest; // return modified dest string
+return dest;
 }
