958bc2bfca013e0989d3d93f4a793b6800e3635f
Nudul <cjsuntken@gmail.com>
2024-06-03 23:22:50 +0000

    t

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -5,19 +5,13 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	char buffer[17];
-
 	for (i = 0; i < 8; i++)
 	{
-		int index = 0;
 		for (j = 0; j < 8; j++)
 		{
-			buffer[index++] = a[i][j];
-			buffer[index++] = ' ';
+			print_char(a[i][j]);
+			print_char(' ');
 		}
-		buffer[index++] = '\n';
-		buffer[index] = '\0';
-
-		write(1, buffer, index);
+		print_char('\n');
 	}
 }
