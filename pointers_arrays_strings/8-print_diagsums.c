696fe34a9611a575955f7d8650b3c46076f3018b
Nudul <cjsuntken@gmail.com>
2024-06-03 23:34:58 +0000

    t

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -6,13 +6,14 @@ void print_diagsums(int *a, int size)
 {
 	int sum_diag1 = 0;
 	int sum_diag2 = 0;
+	int i;
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum_diag1 += a[i * size + i];
 	}
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum_diag2 += a[i * size + (size - 1 - i)];
 	}
