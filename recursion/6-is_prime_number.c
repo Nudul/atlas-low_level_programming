99c51ac8022f367f39fd42407e8e71f53f10ad03
Nudul <cjsuntken@gmail.com>
2024-06-10 22:55:32 +0000

    second test

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,18 +3,14 @@
  * is_prime_number - main function
  * Return: 1 if prime, otherwise 0
  */
-int is_prime_number(int n)
+int is_prime_number(int n, int i)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
-
-	for (i = 2; i * i <= n; i++)
-	{
+	if (i == 1)
+		return (1);
 	if (n % i == 0)
 		return (0);
-	}
-	
-	return (1);
+
+	return is_prime_recursive(n, i - 1);
 }
