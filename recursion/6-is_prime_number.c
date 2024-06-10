bbc26d977b00fece4c1885c0500a3655c353ebc3
Nudul <cjsuntken@gmail.com>
2024-06-10 23:08:32 +0000

    asdlklasdk

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * is_prime_number - main function
- * Return: 1 if prime, otherwise 0
  */
 int is_prime_number(int n)
 {
 	int i;
 
-	if (n <= 1)
-		return (0);
-	if (i == 1)
-		return (1);
-	if (n % i == 0)
-		return (0);
+	for (i = 2; i <= n/2; i++)
+	{
+		if (n%i == 0)
+		{
+			return (0);
+		}
+	}
 
-	return is_prime_number(n, i - 1);
+	return (1);
 }
