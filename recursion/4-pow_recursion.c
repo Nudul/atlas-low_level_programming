65f4eaba35342c5b3e57880609f46760d4d66fdd
Nudul <cjsuntken@gmail.com>
2024-06-10 22:34:29 +0000

    test

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * _pow_recursion - main function to return x to power of y
+ * @x: main value needed
+ * @y: error case -1 since negatives aren't used here
  * Return: -1 negative y. 1 if power of 0 is 1.
  */
 int _pow_recursion(int x, int y)
