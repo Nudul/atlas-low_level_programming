db464947b3d2dbd3f38e1c659f2f95d5918d6569
Nudul <cjsuntken@gmail.com>
2024-06-10 22:21:21 +0000

    betty cleanup

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * int factorial - main factorial function
+ * factorial - main factorial function
  * @n: input number
+ * Return: -1 error case. 1 because 0 factorial is 1.
  */
 int factorial(int n)
 {
