0d82cdc3020f6f932a41d5d18bd0fe15d7178c70
Nudul <cjsuntken@gmail.com>
2024-06-10 22:13:23 +0000

    betty cleanup

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
  * _strlen_recursion - main function for returning string length recursively
+ * @s: points to first character
+ * Return: 0 for when the end of the string is reached. 1 is used for the recursion that goes through each character in the string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 
-	return 1 + _strlen_recursion(s + 1);
+	return (1) + _strlen_recursion(s + 1);
 }
