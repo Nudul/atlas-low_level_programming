33195fd00e66b10f7e8b76d556514b399ce01d1e
Nudul <cjsuntken@gmail.com>
2024-06-10 22:19:03 +0000

    first test

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int factorial - main factorial function
+ * @n: input number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return n * factorial(n - 1);
+}
