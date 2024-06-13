42e97a09e6b14ef657e82b7daa585df6178929f2
Nudul <cjsuntken@gmail.com>
2024-06-13 18:30:49 -0500

    asdf

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+char *create_array(unsigned int size, char c)
+{
+    int size = 0;
+    char *arr = NULL;
+
+    if (size == 0)
+    {
+        return (1);
+    }
+
+    arr = (char *)malloc(size * sizeof(char));
+    if (arr == NULL)
+    {
+        return (1);
+    }
+
+    arr[0] = 'A' + size - 1;
+
+    free(arr);
+
+        return (0);
+}
