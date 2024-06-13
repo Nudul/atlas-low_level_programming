14ebd21ce3bb8003b6d474b8287aaa9c7e3a82b3
Nudul <cjsuntken@gmail.com>
2024-06-13 18:53:50 -0500

    declarationzzz

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,8 +5,8 @@
 char *create_array(unsigned int size, char c)
 {
     char *arr = NULL;
-    int size;
-    int c;
+    unsigned int i;
+    char c;
 
     if (size == 0)
     {
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
         return NULL;
     }
 
-    for (unsigned int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         arr[i] = c;
     }
