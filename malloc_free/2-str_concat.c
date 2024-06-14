9e26c0c700aec0429fa8ea2da4ac822d833bf2f9
Nudul <cjsuntken@gmail.com>
2024-06-13 20:53:23 -0500

    formatting for if statements

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,9 +6,14 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-if (s1 == NULL || s2 == NULL)
-	s1 = "";
-	s2 = "";
+if (s1 == NULL)
+	{
+		s1 = "";
+	}
+if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
 size_t length_s1 = strlen(s1);
 size_t length_s2 = strlen(s2);
