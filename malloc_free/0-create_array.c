7c3c5741c040ed3fd9fe149c0653ac9088d67a7c
Nudul <cjsuntken@gmail.com>
2024-06-13 19:28:46 -0500

    vscode indentation settings change test

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,19 +4,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+	char *array;
+	unsigned int i;
 
-    if (size == 0)
-    return NULL;
+	if (size == 0)
+	return (NULL);
 
-    array = malloc(size * sizeof(*array));
-    if (array == NULL)
-            return NULL;
+	array = malloc(size * sizeof(*array));
+	if (array == NULL)
+	return (NULL);
 
-    for (i = 0; i < size; i++)
-    {
-        array[i] = c;
-    }
-    return (array);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
 }
