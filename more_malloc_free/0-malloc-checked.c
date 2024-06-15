11648731b75234a7e9f5be47cbd1c5149b0e3959
Nudul <cjsuntken@gmail.com>
2024-06-15 15:02:33 -0500

    first code test

---
+++ more_malloc_free/0-malloc-checked.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*
+*/
+void *malloc_checked(unsigned int b)
+{
+
+void *ptr = malloc(b);
+if (ptr == NULL)
+	{
+	exit(98);
+	}
+	return ptr;
+}
