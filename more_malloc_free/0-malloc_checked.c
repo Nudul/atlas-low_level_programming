35f62c479228c64810691f5f98da40970ac5f758
Nudul <cjsuntken@gmail.com>
2024-06-15 15:16:04 -0500

    betty formatting

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*
+* malloc_checked - allocates memory
 */
 void *malloc_checked(unsigned int b)
 {
@@ -12,5 +12,5 @@ if (ptr == NULL)
 	{
 	exit(98);
 	}
-	return ptr;
+	return (ptr);
 }
