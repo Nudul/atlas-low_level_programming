36cb453bb5bd55a9f68713850aa609280bbbee2f
Nudul <cjsuntken@gmail.com>
2024-06-15 16:49:06 -0500

    first draft

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - prints file name
+* @argc: counts arguments
+* @argv: array of ptr to str
+* Return: 0 on success
+*/
+int is_positive_number(const char *str)
+{
+	if (*str == '\0') return (0);
+	while (*str)
+	{
+		if (!isdigit(*str)) return (0);
+		str++;
+	}
+	return (1);
+}
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_positive_number(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+}
