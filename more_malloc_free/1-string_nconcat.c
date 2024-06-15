0944599b8088643ebca36162544922effa9bf6ab
Nudul <cjsuntken@gmail.com>
2024-06-15 15:47:17 -0500

    still messing with declarations

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -6,24 +6,26 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int i = 0;
+	unsigned int i;
+	unsigned int i;
+	unsigned int len1;
+	unsigned int len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	unsigned int len1 = 0;
+	len1 = 0;
 	while (s1[len1] != '\0')
 	len1++;
 
-	unsigned int len2 = 0;
+	len2 = 0;
 	while (s2[len2] != '\0')
 	len2++;
 
 	unsigned int full_len = len1 + ((n < len2) ? n : len2);
-	char *result = (char *)malloc(total_len + 1);
+	char *result = (char *)malloc(full_len + 1);
 
 	if (result == NULL)
 	{
@@ -36,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < n && i < len2; i++)
 	result[len1 + i] = s2[i];
 
-	result[total_len] = '\0';
+	result[full_len] = '\0';
 
 	return (result);
 }
