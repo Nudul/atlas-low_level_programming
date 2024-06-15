055162f3fec338bbdaf418121164350614ba8da1
Nudul <cjsuntken@gmail.com>
2024-06-15 16:31:35 -0500

    -1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,9 +5,9 @@
 * @argv: array of ptr to str
 * Return: 0 on success
 */
-int main(int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
