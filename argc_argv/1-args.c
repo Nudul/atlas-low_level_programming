b44a630055e7f338210c6a9add1dc4c004ab5ed3
Nudul <cjsuntken@gmail.com>
2024-06-15 16:25:09 -0500

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints file name
+* @argc: counts arguments
+* @argv: array of ptr to str
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	printf("%d\n", argc);
+
+	return (0);
+}
