f0e475df86325582daa5abd16840a69a76383a67
Nudul <cjsuntken@gmail.com>
2024-06-17 13:23:10 -0500

    first test

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*/
+int *array_range(int min, int max)
+{
+	int elements = max - min + 1;
+	int *arr;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	arr = (int *)malloc(elements * sizeof(int));
+
+	if (arr == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < elements; i++)
+	{
+		arr[i] = min + i;
+	}
+
+	return (arr);
+}
