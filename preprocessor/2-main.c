23e15631dcbdc264d9f3926e115f8382abe37d9d
Nudul <cjsuntken@gmail.com>
2024-06-17 14:33:34 -0500

    test

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-int main(void)
+char *lastSlash(char *path)
 {
-	printf(__FILE__)
-	return (0);
+	char *lastSlash = NULL;
+	int len = strlen(path);
+
+	for (i = len - 1; i < 0; --i;)
+	{
+		if (path[i] == '/')
+		{
+			lastSlash = &path[i];
+			break;
+		}
+	}
+
+	return lastSlash;
 }
