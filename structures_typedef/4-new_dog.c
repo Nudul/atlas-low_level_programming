71d4f02128fc1b4fc2f921dfd06d1ec00bca1c7b
Nudul <cjsuntken@gmail.com>
2024-06-23 18:18:34 -0500

    free

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* free_dog - frees dogs
+* @d: pointer
+* Return: not needed
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
