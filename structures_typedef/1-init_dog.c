da47d6a50b9b8a0b03f77be402d87d5ed258bde1
Nudul <cjsuntken@gmail.com>
2024-06-23 17:35:50 -0500

    defined struct first

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
