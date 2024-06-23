2b3163a319a43db72b1fa0baf5d700e509e98472
Nudul <cjsuntken@gmail.com>
2024-06-23 17:51:12 -0500

    test

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,15 +2,7 @@
 /**
 *
 */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
-
 {
 	if (d != NULL)
 	{
@@ -19,3 +11,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->owner = owner;
 	}
 }
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
