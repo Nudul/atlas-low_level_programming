adfebd6f3aa15c5a3bfcb5284cd0ef629f69408a
Nudul <cjsuntken@gmail.com>
2024-06-24 15:22:36 -0500

    betty

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+* int_index - array
+* @array: array of ints
+* @size: size of array
+* @cmp: checking elements of array
+*/
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		return (i);
+	}
+	return (-1);
+}
