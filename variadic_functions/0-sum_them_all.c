a6af0c375326956048f2feceb5c4f7d67253cd38
Nudul <cjsuntken@gmail.com>
2024-06-24 15:43:08 -0500

    variadic af

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum the passed parameters
+ * @n: num of parameters
+ * Return: sum of parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i;
+	int sum = 0;
+	va_list args;
+
+	if (n == NULL)
+		return (NULL);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+		return (sum);
+}
